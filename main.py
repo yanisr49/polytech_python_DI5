e94383d5951202404efc5369a30531681e265b05
Yanis <yanisrichard@live.fr>
2019-10-13 00:09:42 +0200

    coding improvements :)

--- main.py
+++ main.py
@@ -6,85 +6,82 @@ class Calculator(Frame):
     def __init__(self, master):
         super().__init__(master)
         self.pack(fill=BOTH)
-        self.master = master
-        self.create_widgets()
-
-        self.historic = ["0"]
-
-    def create_widgets(self):
 
         self.display = Label(self, text="0", anchor=E)
         self.display["background"] = "#AAAAAA"
         self.display["justify"] = "right"
         self.display.grid(row=0, column=0, columnspan=5, sticky="nesw")
 
+        self.create_buttons()
+
+        self.historic = ["0"]
+
+    def create_buttons(self):
+
         # Create all buttons
-        self.button_7 = Button(self, text="7", command= lambda: self.add_historic("7"))
-        self.button_7.grid(row=1, column=0, sticky="nesw")
+        button_7 = Button(self, text="7", command=lambda: self.add_historic("7"))
+        button_7.grid(row=1, column=0, sticky="nesw")
         self.master.bind("<KP_7>", lambda event: self.add_historic("7"))
-        self.button_8 = Button(self, text="8", command= lambda: self.add_historic("8"))
-        self.button_8.grid(row=1, column=1, sticky="nesw")
+        button_8 = Button(self, text="8", command=lambda: self.add_historic("8"))
+        button_8.grid(row=1, column=1, sticky="nesw")
         self.master.bind("<KP_8>", lambda event: self.add_historic("8"))
-        self.button_9 = Button(self, text="9", command= lambda: self.add_historic("9"))
-        self.button_9.grid(row=1, column=2, sticky="nesw")
+        button_9 = Button(self, text="9", command=lambda: self.add_historic("9"))
+        button_9.grid(row=1, column=2, sticky="nesw")
         self.master.bind("<KP_9>", lambda event: self.add_historic("9"))
-        self.button_plus = Button(self, text="+", command= lambda: self.add_historic("+"))
-        self.button_plus.grid(row=1, column=3, sticky="nesw")
+        button_plus = Button(self, text="+", command=lambda: self.add_historic("+"))
+        button_plus.grid(row=1, column=3, sticky="nesw")
         self.master.bind("<KP_Add>", lambda event: self.add_historic("+"))
-        self.button_C = Button(self, text="C", command=self.backspace)
-        self.button_C.grid(row=1, column=4, sticky="nesw")
+        button_C = Button(self, text="C", command=self.backspace)
+        button_C.grid(row=1, column=4, sticky="nesw")
         self.master.bind("<BackSpace>", self.backspace)
 
-
-        self.button_4 = Button(self, text="4", command= lambda: self.add_historic("4"))
-        self.button_4.grid(row=2, column=0, sticky="nesw")
+        button_4 = Button(self, text="4", command=lambda: self.add_historic("4"))
+        button_4.grid(row=2, column=0, sticky="nesw")
         self.master.bind("<KP_4>", lambda event: self.add_historic("4"))
-        self.button_5 = Button(self, text="5", command= lambda: self.add_historic("5"))
-        self.button_5.grid(row=2, column=1, sticky="nesw")
+        button_5 = Button(self, text="5", command=lambda: self.add_historic("5"))
+        button_5.grid(row=2, column=1, sticky="nesw")
         self.master.bind("<KP_5>", lambda event: self.add_historic("5"))
-        self.button_6 = Button(self, text="6", command= lambda: self.add_historic("6"))
-        self.button_6.grid(row=2, column=2, sticky="nesw")
+        button_6 = Button(self, text="6", command=lambda: self.add_historic("6"))
+        button_6.grid(row=2, column=2, sticky="nesw")
         self.master.bind("<KP_6>", lambda event: self.add_historic("6"))
-        self.button_minus = Button(self, text="-", command= lambda: self.add_historic("-"))
-        self.button_minus.grid(row=2, column=3, sticky="nesw")
+        button_minus = Button(self, text="-", command=lambda: self.add_historic("-"))
+        button_minus.grid(row=2, column=3, sticky="nesw")
         self.master.bind("<KP_Subtract>", lambda event: self.add_historic("-"))
-        self.button_AC = Button(self, text="AC", command=self.reset)
-        self.button_AC.grid(row=2, column=4, sticky="nesw")
+        button_AC = Button(self, text="AC", command=self.reset)
+        button_AC.grid(row=2, column=4, sticky="nesw")
         self.master.bind("<Delete>", self.reset)
 
-
-        self.button_1 = Button(self, text="1", command= lambda: self.add_historic("1"))
-        self.button_1.grid(row=3, column=0, sticky="nesw")
+        button_1 = Button(self, text="1", command=lambda: self.add_historic("1"))
+        button_1.grid(row=3, column=0, sticky="nesw")
         self.master.bind("<KP_1>", lambda event: self.add_historic("1"))
-        self.button_2 = Button(self, text="2", command= lambda: self.add_historic("2"))
-        self.button_2.grid(row=3, column=1, sticky="nesw")
+        button_2 = Button(self, text="2", command=lambda: self.add_historic("2"))
+        button_2.grid(row=3, column=1, sticky="nesw")
         self.master.bind("<KP_2>", lambda event: self.add_historic("2"))
-        self.button_3 = Button(self, text="3", command= lambda: self.add_historic("3"))
-        self.button_3.grid(row=3, column=2, sticky="nesw")
+        button_3 = Button(self, text="3", command=lambda: self.add_historic("3"))
+        button_3.grid(row=3, column=2, sticky="nesw")
         self.master.bind("<KP_3>", lambda event: self.add_historic("3"))
-        self.button_time = Button(self, text="*", command= lambda: self.add_historic("*"))
-        self.button_time.grid(row=3, column=3, sticky="nesw")
+        button_time = Button(self, text="*", command=lambda: self.add_historic("*"))
+        button_time.grid(row=3, column=3, sticky="nesw")
         self.master.bind("<KP_Multiply>", lambda event: self.add_historic("*"))
-        self.button_lpar = Button(self, text="(", command= lambda: self.add_historic("("))
-        self.button_lpar.grid(row=3, column=4, sticky="nesw")
+        button_lpar = Button(self, text="(", command=lambda: self.add_historic("("))
+        button_lpar.grid(row=3, column=4, sticky="nesw")
         self.master.bind("(", lambda event: self.add_historic("("))
 
-
-        self.button_0 = Button(self, text="0", command= lambda: self.add_historic("0"))
-        self.button_0.grid(row=4, column=0, sticky="nesw")
+        button_0 = Button(self, text="0", command=lambda: self.add_historic("0"))
+        button_0.grid(row=4, column=0, sticky="nesw")
         self.master.bind("<KP_0>", lambda event: self.add_historic("0"))
-        self.button_dot = Button(self, text=".", command= lambda: self.add_historic("."))
-        self.button_dot.grid(row=4, column=1, sticky="nesw")
+        button_dot = Button(self, text=".", command=lambda: self.add_historic("."))
+        button_dot.grid(row=4, column=1, sticky="nesw")
         self.master.bind("<KP_Decimal>", lambda event: self.add_historic("."))
-        self.button_eq = Button(self, text="=", command=self.display_total)
-        self.button_eq.grid(row=4, column=2, sticky="nesw")
+        button_eq = Button(self, text="=", command=self.display_total)
+        button_eq.grid(row=4, column=2, sticky="nesw")
         self.master.bind("<KP_Enter>", self.display_total)
         self.master.bind("<Return>", self.display_total)
-        self.button_divide = Button(self, text="/", command= lambda: self.add_historic("/"))
-        self.button_divide.grid(row=4, column=3, sticky="nesw")
+        button_divide = Button(self, text="/", command=lambda: self.add_historic("/"))
+        button_divide.grid(row=4, column=3, sticky="nesw")
         self.master.bind("<KP_Divide>", lambda event: self.add_historic("/"))
-        self.button_rpar = Button(self, text=")", command= lambda: self.add_historic(")"))
-        self.button_rpar.grid(row=4, column=4, sticky="nesw")
+        button_rpar = Button(self, text=")", command=lambda: self.add_historic(")"))
+        button_rpar.grid(row=4, column=4, sticky="nesw")
         self.master.bind(")", lambda event: self.add_historic(")"))
 
     def add_historic(self, value):
@@ -122,6 +119,8 @@ class Calculator(Frame):
             for value in self.historic:
                 self.display["text"] += value
 
+
 window = Tk()
+window.title("Calculatrice")
 calc = Calculator(window)
 calc.mainloop()
