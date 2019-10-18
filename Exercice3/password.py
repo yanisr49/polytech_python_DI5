c75817b0150be9e63db3d381f7838e4f32878ecc
Yanis <yanisrichard@live.fr>
2019-10-18 10:05:06 +0200

    exercice 3

--- Exercice3/password.py
+++ Exercice3/password.py
@@ -1,17 +1,20 @@
 import hashlib
 import getpass
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 
 
 class Password(object):
 
     def __init__(self, filename):
         self.filename = filename
+        self.key = None
 
         while self.ask_action():
             print("\n---------------------------------------------------------------------")
 
     def ask_action(self):
-        print("\n1 - login\n2 - sign in\n3 - leave")
+        print("\n1 - login\n2 - sign in\n3 - encrypt file\n4 - decrypt file\n5 - leave")
         action = input("=> ")
 
         if action == "1":
@@ -23,6 +26,22 @@ class Password(object):
             password = getpass.getpass("User password : \n=> ")
             self.connect(username, password)
         elif action == "3":
+            path = input("path to the file : \n=> ")
+            self.key = get_random_bytes(16)
+            cipher = AES.new(self.key, AES.MODE_EAX)
+            with open(path, mode='rb') as file:  # b is important -> binary
+                fileContent = file.read()
+                ciphertext, tag = cipher.encrypt_and_digest(fileContent)
+            with open("encrypted_"+path, "wb") as file_out:
+                [file_out.write(x) for x in (cipher.nonce, tag, ciphertext)]
+        elif action == "4":
+            path = input("path to the file : \n=> ")
+            with open(path, "rb") as file_in:
+                nonce, tag, ciphertext = [file_in.read(x) for x in (16, 16, -1)]
+                cipher = AES.new(self.key, AES.MODE_EAX, nonce)
+                data = cipher.decrypt_and_verify(ciphertext, tag)
+                print(data)
+        elif action == "5":
             return False
         return True
 
