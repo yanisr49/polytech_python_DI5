80db0685915e6bee5c61765b01c8cede22c22de2
Yanis <yanisrichard@live.fr>
2019-11-15 16:50:42 +0100

    :)

--- Exercice5/CSVParser.py
+++ Exercice5/CSVParser.py
@@ -3,11 +3,20 @@ import csv
 
 class Parser(object):
 
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self):
+        print("log - Parser, created")
 
-    def get_data(self):
-        with open(self.filename, newline='') as csvfile:
+    def get_regions(self):
+        with open("bdds/regions.csv", newline='') as csvfile:
             spamreader = csv.reader(csvfile, delimiter=';')
-            i = 0
-            return [row for row in spamreader]
+            return [row[0:2] for row in spamreader][8:]
+
+    def get_departements(self):
+        with open("bdds/departements.csv", newline='') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=';')
+            return [row[0:3] for row in spamreader][8:]
+
+    def get_communes(self):
+        with open("bdds/communes.csv", newline='') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=';')
+            return [[row[0], row[2], row[6], row[9]] for row in spamreader][8:]
--- Exercice5/bdd.py
+++ Exercice5/bdd.py
@@ -8,25 +8,26 @@ class Database(object):
         self.db = sqlite3.connect('bdds/mydb.db')
         print('log - Connected to the databse')
         self.c = self.db.cursor()
+        self.parser = Parser()
 
     def reset_tables(self):
         self.c.execute('''DROP TABLE IF EXISTS region''')
         self.db.commit()
 
     def create_table_region(self):
-        self.c.execute('''CREATE TABLE region (code, name, nb_arr, nb_canton, nb_commune, pop_mun, pop_tot)''')
+        self.c.execute('''CREATE TABLE region (code, name)''')
         print('log - Table region created')
         self.db.commit()
 
     def add_all_regions(self):
-        self.c.executemany('INSERT INTO region VALUES (?, ?, ?, ?, ?, ?, ?)', Parser('bdds/regions.csv').get_data()[8:])
+        self.c.executemany('INSERT INTO region VALUES (?, ?)', Parser().get_regions())
         self.db.commit()
-        print('log - '+str(len(Parser('bdds/regions.csv').get_data()[8:]))+' row inserted')
+        print('log - '+str(len(Parser().get_regions()))+' row inserted')
 
 
     def add_region(self, row):
         self.c.execute("INSERT INTO region VALUES ("
-                       "'"+row[0]+"', '"+row[1]+"', '"+row[2]+"', '"+row[3]+"', '"+row[4]+"', '"+row[5]+"', '"+row[6]+"')")
+                       "'"+row[0]+"', '"+row[1]+"')")
         self.db.commit()
 
     def get_pop_tot(self):
@@ -49,5 +50,4 @@ database = Database()
 database.reset_tables()
 database.create_table_region()
 database.add_all_regions()
-print(database.get_pop_tot())
 database.close()
