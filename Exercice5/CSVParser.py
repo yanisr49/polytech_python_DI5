5d53c06747b742d40cd3a145d64f6a46f0a6fb63
Paul <paul.retail@me.com>
2019-11-15 17:18:01 +0100

    exercice 5

--- Exercice5/CSVParser.py
+++ Exercice5/CSVParser.py
@@ -9,12 +9,12 @@ class Parser(object):
     def get_regions(self):
         with open("bdds/regions.csv", newline='') as csvfile:
             spamreader = csv.reader(csvfile, delimiter=';')
-            return [[int(row[0]), str(row[1])] for row in spamreader][8:]
+            return [[row[0], row[1]] for row in spamreader][8:]
 
     def get_departements(self):
         with open("bdds/departements.csv", newline='') as csvfile:
             spamreader = csv.reader(csvfile, delimiter=';')
-            return [int([row[2]), str(row[3]), int(row[0])] for row in spamreader][8:]
+            return [[row[2], row[3], int(row[0])] for row in spamreader][8:]
 
     def get_communes(self):
         with open("bdds/communes.csv", newline='') as csvfile:
--- Exercice5/bdd.py
+++ Exercice5/bdd.py
@@ -6,48 +6,83 @@ class Database(object):
 
     def __init__(self):
         self.db = sqlite3.connect('bdds/mydb.db')
-        print('log - Connected to the databse')
+        print('log - Connected to the database')
         self.c = self.db.cursor()
         self.parser = Parser()
 
+    def create_all(self):
+        self.reset_tables()
+        self.create_table_region()
+        self.create_table_departement()
+        self.create_table_commune()
+        self.add_all_regions()
+        self.add_all_departements()
+        self.add_all_communes()
+        self.print_regions()
+        self.print_departements()
+        self.print_communes()
+        self.close()
+
     def reset_tables(self):
         self.c.execute('''DROP TABLE IF EXISTS region''')
+        self.c.execute('''DROP TABLE IF EXISTS departement''')
+        self.c.execute('''DROP TABLE IF EXISTS commune''')
         self.db.commit()
 
     def create_table_region(self):
-        self.c.execute('''CREATE TABLE region (code, name)''')
+        self.c.execute('''CREATE TABLE region (code_region integer PRIMARY KEY, name text NOT NULL)''')
         print('log - Table region created')
         self.db.commit()
 
+    def create_table_departement(self):
+        self.c.execute(
+            '''CREATE TABLE departement (code_departement integer PRIMARY KEY, name text NOT NULL, code_region integer, FOREIGN KEY(code_region) REFERENCES region(code_region))''')
+        print('log - Table departement created')
+        self.db.commit()
+
+    def create_table_commune(self):
+        self.c.execute(
+            '''CREATE TABLE commune (code_commune integer PRIMARY KEY, name text NOT NULL, pop_tot integer,code_departement integer, FOREIGN KEY(code_departement)REFERENCES departement(code_departement))''')
+        print('log - Table commune created')
+        self.db.commit()
+
     def add_all_regions(self):
-        self.c.executemany('INSERT INTO region VALUES (?, ?)', Parser().get_regions())
+        self.c.executemany('INSERT INTO region VALUES (?, ?)', self.parser.get_regions())
         self.db.commit()
-        print('log - '+str(len(Parser().get_regions()))+' row inserted')
+        print('log - ' + str(len(self.parser.get_regions())) + ' regions inserted')
 
-
-    def add_region(self, row):
-        self.c.execute("INSERT INTO region VALUES ("
-                       "'"+row[0]+"', '"+row[1]+"')")
+    def add_all_departements(self):
+        print(self.parser.get_departements()[0])
+        self.c.executemany('INSERT INTO departement VALUES (?, ?, ?)', self.parser.get_departements())
         self.db.commit()
+        print('log - ' + str(len(self.parser.get_departements())) + ' departements inserted')
 
-    def get_pop_tot(self):
-        total = 0
-        for row  in self.c.execute('SELECT pop_tot FROM region'):
-            total = total + int(row[-1].replace(" ", ""))
-        return total
+    def add_all_communes(self):
+        self.c.executemany('INSERT INTO commune VALUES (?, ?, ?, ?)', self.parser.get_communes())
+        self.db.commit()
+        print('log - ' + str(len(self.parser.get_communes())) + ' communes inserted')
 
-    def print_region(self):
-        t = ('RHAT',)
+    def print_regions(self):
         self.c.execute('SELECT * FROM region ORDER BY name')
         for row in self.c.fetchall():
             print(row)
-        print('log - '+str(len(self.c.execute('SELECT * FROM region').fetchall()))+' rows found')
+        print('log - ' + str(len(self.c.execute('SELECT * FROM region').fetchall())) + ' regions found')
+
+    def print_departements(self):
+        self.c.execute('SELECT * FROM departement ORDER BY name')
+        for row in self.c.fetchall():
+            print(row)
+        print('log - ' + str(len(self.c.execute('SELECT * FROM departement').fetchall())) + ' departements found')
+
+    def print_communes(self):
+        self.c.execute('SELECT * FROM commune ORDER BY name')
+        for row in self.c.fetchall():
+            print(row)
+        print('log - ' + str(len(self.c.execute('SELECT * FROM commune').fetchall())) + ' communes found')
 
     def close(self):
         self.db.close()
 
+
 database = Database()
-database.reset_tables()
-database.create_table_region()
-database.add_all_regions()
-database.close()
+database.create_all()
