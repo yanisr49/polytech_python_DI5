173551f354ab656257e67fdbfae2c840974e6c0f
Yanis <yanisrichard@live.fr>
2019-11-15 17:12:47 +0100

    :)

--- Exercice5/CSVParser.py
+++ Exercice5/CSVParser.py
@@ -9,14 +9,14 @@ class Parser(object):
     def get_regions(self):
         with open("bdds/regions.csv", newline='') as csvfile:
             spamreader = csv.reader(csvfile, delimiter=';')
-            return [row[0:2] for row in spamreader][8:]
+            return [[int(row[0]), str(row[1])] for row in spamreader][8:]
 
     def get_departements(self):
         with open("bdds/departements.csv", newline='') as csvfile:
             spamreader = csv.reader(csvfile, delimiter=';')
-            return [[row[2], row[3], row[0]] for row in spamreader][8:]
+            return [int([row[2]), str(row[3]), int(row[0])] for row in spamreader][8:]
 
     def get_communes(self):
         with open("bdds/communes.csv", newline='') as csvfile:
             spamreader = csv.reader(csvfile, delimiter=';')
-            return [[row[5], row[6], row[9], row[2]] for row in spamreader][8:]
+            return [[int(row[5]), str(row[6]), int(row[9]), int(row[2])] for row in spamreader][8:]
